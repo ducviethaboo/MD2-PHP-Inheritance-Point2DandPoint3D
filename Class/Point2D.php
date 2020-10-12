b428591d9136dbf5e41000be6f0ee126aa74c7c6
ducviethaboo <70750327+ducviethaboo@users.noreply.github.com>
2020-10-12 22:05:13 +0700

    Commit 1

---
+++ Class/Point2D.php
@@ -0,0 +1,56 @@
+<?php
+
+
+class Point2D
+{
+    public $x;
+    public $y;
+
+    public function __construct($_x, $_y)
+    {
+        $this->x = $_x;
+        $this->y = $_y;
+    }
+
+    
+    public function getX()
+    {
+        return $this->x;
+    }
+
+
+    public function setX($x)
+    {
+        $this->x = $x;
+    }
+
+
+    public function getY()
+    {
+        return $this->y;
+    }
+
+
+    public function setY($y)
+    {
+        $this->y = $y;
+    }
+
+    public function setXY($x,$y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getXY()
+    {
+        return [$this->x,$this->y];
+    }
+
+    public function toString()
+    {
+        return '('.$this->x.','.$this->y.')';
+    }
+
+
+}
